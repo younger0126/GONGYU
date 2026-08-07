6d723d59138fdcab050c144e2f594f1e45dedfdb
younger0126 <younger0126@gmail.com>
2026-08-08 02:41:10 +0800

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,10 @@
+export const CONFIG = {
+  LIFF_ID: '2011020275-Vs0MIBis',
+
+  // 部署 GAS Web App 後，把 /exec 網址貼在這裡。
+  // 例如：
+  // API_URL: 'https://script.google.com/macros/s/XXXX/exec'
+  API_URL: '',
+
+  APP_VERSION: '0.1.0-sprint1'
+}
